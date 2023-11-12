fa2c859a9216e5a03a697c5e5a3febb8e4a1ef28
nielsenlu2 <nielsen101004@gmail.com>
2023-11-12 10:22:16 -0700

    updated timer

--- blog/ComputerSimulator/Windows/ComingSoon/script.js
+++ blog/ComputerSimulator/Windows/ComingSoon/script.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+var countDownDate = new Date("Nov 20, 2023 01:00:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
